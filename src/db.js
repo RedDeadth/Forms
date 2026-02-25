443b9743e9914e2c948eade6c59c9c65a37c6191
redDeadth <mimamamemima335@gmail.com>
2026-02-25 12:58:17 -0500

    mejoras mejoras

--- src/db.js
+++ src/db.js
@@ -2,9 +2,9 @@ import mysql from 'mysql2/promise';
 
 export const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root', // Default XAMPP user
-    password: '', // Default XAMPP empty password
-    database: 'viviendas_db', // New Database
+    user: 'root',
+    password: '',
+    database: 'viviendas_db',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
